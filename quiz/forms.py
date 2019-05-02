cdb9f7a24cb31e1417619b71e8259c00fd35c2ae
mayers <mayers@ganesha>
2019-05-02 08:53:49 +0200

    Change quiz.forms.py field and widget to multichoice

--- quiz/forms.py
+++ quiz/forms.py
@@ -1,13 +1,13 @@
 from django import forms
-from django.forms.widgets import RadioSelect, Textarea
+from django.forms.widgets import CheckboxSelectMultiple, Textarea
 
 
 class QuestionForm(forms.Form):
     def __init__(self, question, *args, **kwargs):
         super(QuestionForm, self).__init__(*args, **kwargs)
         choice_list = [x for x in question.get_answers_list()]
-        self.fields["answers"] = forms.ChoiceField(choices=choice_list,
-                                                   widget=RadioSelect)
+        self.fields["answers"] = forms.MultipleChoiceField(choices=choice_list,
+                                                  widget=CheckboxSelectMultiple)
 
 
 class EssayForm(forms.Form):
